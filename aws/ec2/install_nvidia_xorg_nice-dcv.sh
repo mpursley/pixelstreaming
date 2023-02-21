90f80dfdaf65e0c2671cf6a2a4cacce8aa82bbe7
mpursley <mpursley@gmail.com>
2023-02-21 11:21:12 -0800

    Update install_nvidia_xorg_nice-dcv.sh

--- aws/ec2/install_nvidia_xorg_nice-dcv.sh
+++ aws/ec2/install_nvidia_xorg_nice-dcv.sh
@@ -31,7 +31,10 @@ if [[ ! -f NVIDIA-Linux-x86_64-520.56.06.run ]] ; then
   wget 'https://www.nvidia.com/content/DriverDownloads/confirmation.php?url=/XFree86/Linux-x86_64/520.56.06/NVIDIA-Linux-x86_64-520.56.06.run'
 fi
 sudo chmod +x NVIDIA-Linux-x86_64*.run
-sudo /bin/sh ./NVIDIA-Linux-x86_64*.run -q
+if [[ "$(modinfo /usr/lib/modules/$(uname -r)/kernel/drivers/video/nvidia.ko | grep ^version)" != 'version:        520.56.06' ]] ; then
+  echo "Nvidia driver is not installed.  Installing it now..."
+  echo sudo /bin/sh ./NVIDIA-Linux-x86_64*.run -q
+fi
 nvidia-smi -q | head# Configure the X Server
 sudo systemctl set-default graphical.target
 sudo systemctl isolate graphical.target
@@ -47,22 +50,32 @@ wget https://d1uj6qtbmh3dt5.cloudfront.net/NICE-GPG-KEY
 gpg --import NICE-GPG-KEY
 sudo rm NICE-GPG-KEY
 sudo dcvstartx &
-sudo wget https://d1uj6qtbmh3dt5.cloudfront.net/2022.0/Servers/nice-dcv-2022.0-12123-ubuntu2004-x86_64.tgz
-sudo tar xvzf nice-dcv-*ubun*.tgz && cd nice-dcv-*64
+if [[ ! -f nice-dcv-2022.0-12123-ubuntu2004-x86_64.tgz ]] ; then
+  echo "nice-dcv-2022.0-12123-ubuntu2004-x86_64.tgz doesn't exist, downloading it now"
+  sudo wget https://d1uj6qtbmh3dt5.cloudfront.net/2022.0/Servers/nice-dcv-2022.0-12123-ubuntu2004-x86_64.tgz
+fi
+if [[ ! -d nice-dcv-2022.0-12123-ubuntu2004-x86_64 ]] ; then
+  sudo tar xvzf nice-dcv-*ubun*.tgz
+fi
+cd nice-dcv-2022.0-12123-ubuntu2004-x86_64
 sudo apt install -y ./nice-dcv-ser*.deb
 sudo apt install -y ./nice-x*.deb
 sudo apt install -y ./nice-dcv-web*.deb
 sudo usermod -aG video ubuntu
 sudo usermod -aG video dcv
 # Start the DCV Server and DCV session
-#sudo systemctl enable dcvserver
-#sudo systemctl start dcvserver
-#sudo dcv create-session --type=console --owner=ubuntu newsession
-#sudo dcv list-sessions
-#sudo dcv close-session newsession
+sudo systemctl enable dcvserver
+sudo systemctl start dcvserver
+sudo dcv create-session --type=console --owner=ubuntu newsession
+sudo dcv list-sessions
+sudo dcv close-session newsession
 # Create random password for the user Ubuntu
 #PASS=$(aws secretsmanager get-random-password --require-each-included-type --password-length 20 --query RandomPassword)
 #INSTANCE=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/public-hostname)
 #aws secretsmanager create-secret --name DCV/$INSTANCE --description "Credentials for $INSTANCE." --secret-string "{\"user\":\"ubuntu\",\"password\":$PASS}"
 sudo systemctl isolate multi-user.target
 sudo systemctl isolate graphical.target
+
+if [[ ! -d ~/PixelStreaming ]] ; then
+  echo "WARNING: The PixelStreaming folder doesn't existing.  Please download and extract the PixelStreaming.tgz project file..."
+fi
